8e5c66f12c44613cbfa8f9feae6dc297ff3f6199
Aji Prakosa <aji@jelajahrumah.id>
2024-12-07 02:02:02 +0700

    Add ability to serve blade template directly without controller. So we can have simple static page. example: /page/sample-static-page
    Change-Id: If78068ad9293e4400402b7e495659acb18d11c5a

--- samplekit-laravel/routes/web.php
+++ samplekit-laravel/routes/web.php
@@ -7,3 +7,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [ListingController::class, 'index'])->name('listings.index');
 Route::get('/listings/{listing}', [ListingController::class, 'show'])->name('listings.show');
 Route::post('/receipts', [ReceiptController::class, 'store'])->name('receipts.store');
+
+Route::get('/page/{page}', function ($page) {
+    try {
+        return view(sprintf('pages.%s', $page));
+    } catch (\Throwable $th) {
+        abort(404, 'Page not found');
+    }
+});
